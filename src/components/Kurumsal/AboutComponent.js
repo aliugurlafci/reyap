69033a0754c18fc5204a05d2c32283ccd4c0a15b
gokhanyildiz95 <gokhanyildiz95@hotmail.com>
2023-01-11 02:29:03 +0300

    dil seçenekleri butona bağlandı

--- src/components/Kurumsal/AboutComponent.js
+++ src/components/Kurumsal/AboutComponent.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Row, Col } from 'antd';
+import i18n from '../../i18n';
 
 export const AboutComponent = ({ config }) => {
     return (
         <div className="about-container">
             <Row wrap justify="center" style={{ marginBottom: 25, padding: 30 }} gutter={16}>
                 <Row className="about-header-container">
-                    <span className='about-header'>Kurumsal</span>
+                    <span className='about-header'>{i18n.t('institutional')}</span>
                     <Col xs={24} sm={24} md={10} lg={10}>
                         <img src={config.image} alt="instituonal" className='about-image'
                         />
